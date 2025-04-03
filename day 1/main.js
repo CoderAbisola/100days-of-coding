de1c610cedce06abad8500b6169a51ac5da1076f
CoderAbisola <ojoabisola780@gmail.com>
2025-04-03 11:25:08 +0100

    day one

---
+++ day 1/main.js
@@ -0,0 +1,7 @@
+document.getElementById("myButton").addEventListener("click", clicked);
+
+// document.getElementById("myButton").addEventListener("click", function(){alert("button clicked!");})
+
+function clicked(){
+    alert ("button cliceee!")
+}
