45cafea10103a7a58509b6733b17bf0e5d2f7b0a
CoderAbisola <ojoabisola780@gmail.com>
2025-04-08 15:17:00 +0100

    day two

--- day 1/main.js
+++ day 1/main.js
@@ -3,5 +3,5 @@ document.getElementById("myButton").addEventListener("click", clicked);
 // document.getElementById("myButton").addEventListener("click", function(){alert("button clicked!");})
 
 function clicked(){
-    alert ("button cliceee!")
+    alert ("button clicked!")
 }
